c65d4f7f7f24d91f32e6f7844a8c7a12c2806d33
ilovezerocokeya <147008077a@gmail.com>
2024-05-21 20:31:27 +0900

    boxUpdate

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,12 +1,11 @@
 import React from 'react'
+import "./Square.css"
 
-const square = ({ onClick, value }) => {
+
+const Square = ({ onClick, value }) => {
   return (
-    <button className='square'
-        onClick={onClick}>
-        {value}
-    </button>
+    <button className='square' onClick={onClick}> {value} </button>
   )
 }
 
-export default square;
+export default Square;
